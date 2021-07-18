aff2c515a6dd8c8309e6b1113034521bc3e1912d
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-07-18 22:23:17 +0300

    functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,56 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+    let min = max = sum = avg = arr[0];
 
-  // Ваш код
+    for (let i = 1; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+        sum += arr[i];
+    }
 
-  return { min:min, max:max, avg:avg };
+    avg = (sum / arr.length).toFixed(2);
+
+    return {min: min, max: max, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
 
-  // Ваш код
-
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+    let max = 0;
+    for (let i = 0; i < arrOfArr.length; i++) {
+        let result = func(arrOfArr[i]);
+        if (max < result) {
+            max = result;
+        }
+    }
 
-  // Ваш кода
-  // for ...
-  
-  return max
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let min = max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+
+    return max - min;
 }
