d0e01999e8b3c868110c3a90904dd997db77f6a5
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-08-11 13:57:33 +0300

    decorators

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,50 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cashe = {};
+    let counter = 0;
+
+    function wrapper(...args) {
+        const hash = args.join(",");
+        if (hash in cashe) {
+            return "Из кэша: " + cashe[hash];
+        } else {
+            if (counter >= 5) {
+                for (let key in cashe) {
+                    delete cashe[key];
+                    break;
+                }
+            }
+            counter++;
+            cashe[hash] = func(...args);
+            return "Вычисляем: " + cashe[hash];
+        }
+    }
+
+    return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, timeout) {
+    let flag = false;
+    return function (...args) {
+        if (flag) return;
+        func.apply(this, args);
+        flag = true;
+        setTimeout(function () {
+            func.apply(this, args);
+        }, timeout);
+    }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, timeout) {
+    let flag = false;
+    function wrapper(...args) {
+        if (flag) return;
+        wrapper.count++;
+        func.apply(this, args);
+        flag = true;
+        setTimeout(function () {
+            func.apply(this, args);
+        }, timeout);
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
